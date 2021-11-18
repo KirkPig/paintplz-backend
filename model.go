358c0c5138ac7ff40ee0d759f0bd1823d830bc48
Supakorn <supakornmanza21@gmail.com>
2021-11-18 23:37:59 +0700

    added requests

--- model.go
+++ model.go
@@ -1,1 +1,28 @@
 package main
+
+type RegisterRequest struct {
+	Username     string `json:"username" binding:"required"`
+	Name         string `json:"name" binding:"required"`
+	Surname      string `json:"surname" binding:"required"`
+	Email        string `json:"email" binding:"required"`
+	CitizenID    string `json:"citizenID" binding:"required"`
+	Password     string `json:"password" binding:"required"`
+	UserType     bool   `json:"userType" binding:"required"`
+	MinPriceRate int64  `json:"minPriceRate"`
+	MaxPriceRate int64  `json:"maxPriceRate"`
+	Biography    string `json:"biography"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type SearchArtistRequst struct {
+	ArtistName   string   `json:"artistName" binding:"required"`
+	MinPriceRate int64    `json:"minPriceRate"`
+	MaxPriceRate int64    `json:"maxPriceRate"`
+	MinRating    float32  `json:"minRating"`
+	MaxRating    float32  `json:"maxRating"`
+	Tags         []string `json:"tags"`
+}
