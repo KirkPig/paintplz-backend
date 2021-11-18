8ef3ce364d8f92414bec7d417190c28b2c638d9b
Supakorn <supakornmanza21@gmail.com>
2021-11-18 23:45:35 +0700

    fixed request

--- model.go
+++ model.go
@@ -19,10 +19,22 @@ type LoginRequest struct {
 }
 
 type SearchArtistRequst struct {
-	ArtistName   string   `json:"artistName" binding:"required"`
-	MinPriceRate int64    `json:"minPriceRate"`
-	MaxPriceRate int64    `json:"maxPriceRate"`
-	MinRating    float32  `json:"minRating"`
-	MaxRating    float32  `json:"maxRating"`
-	Tags         []string `json:"tags"`
+	ArtistName   string  `json:"artistName" binding:"required"`
+	MinPriceRate int64   `json:"minPriceRate"`
+	MaxPriceRate int64   `json:"maxPriceRate"`
+	MinRating    float32 `json:"minRating"`
+	MaxRating    float32 `json:"maxRating"`
+	Tags         []Tag   `json:"tags"`
+}
+
+type Tag struct {
+	TagId   string `json:"tagID" binding:"required"`
+	TagName string `json:"tagName" binding:"required"`
+}
+
+type SearchResultResponse struct {
+	Username string  `json:"username" binding:"required"`
+	Name     string  `json:"name" binding:"required"`
+	Surname  string  `json:"surname" binding:"required"`
+	Rating   float32 `json:"rating"`
 }
