32928e7433af834d818f3ded7489a5f4e9985577
Supakorn <supakornmanza21@gmail.com>
2021-11-19 21:20:57 +0700

    added API

--- services/handler.go
+++ services/handler.go
@@ -1,1 +1,35 @@
 package services
+
+import "github.com/gin-gonic/gin"
+
+func RegisterHandler(c *gin.Context) {
+
+}
+
+func LoginHandler(c *gin.Context) {
+
+}
+
+func SearchArtistHandler(c *gin.Context) {
+
+}
+
+func GetArtistProfileHandler(c *gin.Context) {
+
+}
+
+func UploadArtworkHandler(c *gin.Context) {
+
+}
+
+func EditArtworkHandler(c *gin.Context) {
+
+}
+
+func DeleteArtworkHandler(c *gin.Context) {
+
+}
+
+func GetTagsHandler(c *gin.Context) {
+
+}
