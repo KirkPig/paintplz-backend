402689cc80a04e894e67c650113d090949582703
KirkPig <KirkPig@github.com>
2021-11-20 17:14:10 +0700

    add model register for db

--- repository/model.go
+++ repository/model.go
@@ -1,5 +1,16 @@
 package repository
 
+type RegisterDBResponse struct {
+	UserID    string `json:"userID"`
+	Username  string `json:"username"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Email     string `json:"email"`
+	CitizenID string `json:"citizenID"`
+	ImageUrl  string `json:"imageUrl"`
+	UserType  string `json:"userType"`
+}
+
 type LoginDBResponse struct {
 	UserID       string  `json:"userID"`
 	Username     string  `json:"username"`
